ac4ddf1a10cd2add04b9efcaa35dd28ab808d26b
sudheerkuntumalla <sudheerkuntumalla3004@gmail.com>
2025-08-04 20:33:07 +0530

    main3

---
+++ main.py
@@ -0,0 +1,6 @@
+n = int(input("Enter the value of n"))
+
+print("Numbers from {0} to {1} are: ".format(n,1))
+
+for i in range(0,n, 1):
+    print(i)
---
+++ main3.py
@@ -0,0 +1,9 @@
+n = int(input("Enter the value of n"))
+
+sum = 0
+i = 1
+while i<=1:
+    sum = sum+i
+    i = i+1
+    
+print("\n sum =", sum)
